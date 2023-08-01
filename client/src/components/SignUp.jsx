8dd0a80047b8bdf023d56f3a2fbff28d0443eb34
Ini1234 <iniabasi.e@yahoo.com>
2023-07-31 21:14:12 -0400

    use correct api route

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -27,7 +27,7 @@ const SignUp = () => {
     event.preventDefault();
     dispatch({ type: 'LOGIN_START' });
     try {
-      const res = await axios.post('/api/v1/auth/login', credentials);
+      const res = await axios.post('/api/v1/auth/register', credentials);
       dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
       navigate('/myspace');
     } catch (err) {
